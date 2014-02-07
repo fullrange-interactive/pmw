7b0a53dbdddc43507f573d7699748b156df6aaf0
Kevin <kevin@henzer.ch>
2014-02-07 21:08:18 +0100

    Added experiment with ntp sync

---
+++ client/node/relems/timeSync.js
@@ -0,0 +1,47 @@
+exports.class = {
+    type:'TimeSync',
+    currentColor:'FFFFFF',
+    draw:function(ctx)
+    {
+        ctx.globalAlpha = 1;
+        ctx.fillStyle='#'+this.currentColor;
+        ctx.fillRect(this.left,this.top,this.width,this.height);
+        ctx.globalAlpha = 1;
+    },
+    drawZone:function(ctx,x,y,width,height)
+    {
+        ctx.globalAlpha = 1;
+        ctx.fillStyle='#'+this.currentColor;
+        ctx.fillRect(x,y,width,height);
+        ctx.globalAlpha = 1;
+    },
+    changeColor:function()
+    {
+        var that = this;
+        
+        
+        console.log(".");
+        if(this.currentColor == 'FFFFFF')
+        {
+            this.currentColor = '000000';
+        }
+        else
+            this.currentColor = 'FFFFFF';
+        
+        this.needRedraw = true;
+        
+        console.log("cg"+(1000-((new Date).getTime())%1000));
+        setTimeout(function(){that.changeColor(that)},(1000-((new Date).getTime())%1000));
+    },
+    load:function(callback){
+        var that = this;
+        console.log("cg"+(1000-((new Date).getTime())%1000));
+        setTimeout(function(){
+            that.changeColor();
+        },(1000-((new Date).getTime())%1000));
+        
+        this.isReady = true;
+        
+        callback();
+    }
+};
