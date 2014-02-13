79eb9963f96b52b7e8395c75fed5b1f8173271c2
floriansegginger <f@floriansegginger.ch>
2014-02-13 21:58:30 +0100

    Window Models operational

--- server/routes/newWindowModel.js
+++ server/routes/newWindowModel.js
@@ -5,6 +5,7 @@ exports.index = function (req, res){
 	newWindowModel.name = req.query.name;
 	newWindowModel.cols = [0.2,0.2,0.2,0.2,0.2];
 	newWindowModel.rows = [0.25,0.25,0.25,0.25];
+	newWindowModel.user = req.user._id;
 	newWindowModel.save(function (err, windowModel){
 		if ( err ) res.send(err);
 		res.send("Ok");
