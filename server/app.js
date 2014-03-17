5d058d7ce612497d93d6560371fdcbafaaf8e135
floriansegginger <f@floriansegginger.ch>
2014-03-17 21:54:33 +0100

    bugtest

--- server/app.js
+++ server/app.js
@@ -234,6 +234,7 @@ clientsServer.on('connection', function(client) {
         
             Window.findOne({windowId:windowId},function(error,window){
 				/* first thing to do is send the windowmodel */
+				console.log("windowmodel id = " + window.windowModel);
 				WindowModel.findById(window.windowModel,function (error, windowModel){
 					console.log(windowModel);
 					client.send(JSON.stringify({type:'windowModel', windowModel:windowModel}))
