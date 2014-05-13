a4ceb4f39ab265c493c1be0534144767f04d5e01
floriansegginger <f@floriansegginger.ch>
2014-05-14 00:07:46 +0200

    Improved performance on index/quick preview

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -1,17 +1,33 @@
 slides = [];
 windowModels = [];
 grids = [];
+function resizeGrid(that, windowModel){
+	var columnsMasksList = new Array();
+	var rowsMasksList = new Array();
+	var nColumns = windowModel.cols.length;
+	var nRows = windowModel.rows.length;
+	$(that).height($(that).width()/windowModel.ratio);
+	$(that).parent(".renderer_wrapper").height($(that).height());
+	$(that).parent(".viewer_wrapper").width($(that).width());
+	$(that).parent(".viewer_wrapper").height($(that).height());
+	$(that).parents(".thumbnail").css("top",($(that).parents(".thumbnail").height()+10)*$(that).attr("window-y"))
+	$(that).parents(".thumbnail").css("left",($(that).parents(".thumbnail").width()+10)*$(that).attr("window-x"))
+	grid.dom = that;
+	if ( $(that).attr("window-id") ){
+		grids[$(that).attr("window-id")] = grid;
+	}else{
+		grids[$(that).attr("slide-id")] = grid;
+	}
+	//var mask = $('<div class="mask-image">');
+	//$(this).append(mask);
+	//grids[$(this).attr('id')].newRelem(0,0,1,1,'Color','front',{color:"FF0000"})
+}
+
 function createGrid(that, windowModel){
 	var columnsMasksList = new Array();
 	var rowsMasksList = new Array();
 	var nColumns = windowModel.cols.length;
 	var nRows = windowModel.rows.length;
-	for(var x = 0; x < nColumns; x++){
-	    columnsMasksList.push(false);
-	}
-	for(var y = 0; y < nRows; y++){
-	    rowsMasksList.push(false);
-	}
 	$(that).height($(that).width()/windowModel.ratio);
 	$(that).parent(".renderer_wrapper").height($(that).height());
 	$(that).parent(".viewer_wrapper").width($(that).width());
@@ -54,6 +70,68 @@ function addRelems (grid, data){
 	});
 }
 
+function getSlideInCache(slideId, callback){
+    if ( slides[slideId] ){
+        var retSlide = {};
+        $.extend(retSlide,slides[slideId]);
+        callback(retSlide);
+    }else{
+        $.getJSON("/slide",{id:slideId},function(slide){
+            slides[slide._id] = slide;
+            var retSlide = {};
+            $.extend(retSlide,slides[slideId]);
+            callback(retSlide);
+        });
+    }
+}
+
+function createCanvasForWrapperLight(){
+	var that = this;
+	var group = $(that).parents(".group");
+	$(that).width(group.width()/group.attr("group-width")-10)
+	if ( !slides[$(this).attr('id')] ){
+		if ( !windowModels[$(that).attr("window-model")]){
+			$.getJSON("/windowModel",{id:$(that).attr("window-model")}, function (windowModel){
+				if ( $(that).hasClass("window_canvas") ){
+					resizeGrid(that,windowModel);
+				}else{
+					var rows = windowModel.rows;
+					var cols = windowModel.cols;
+					var newRows = []
+					var newCols = [];
+					var width = $(that).attr("slide-width");
+					var height = $(that).attr("slide-height");
+                    
+					for ( var y = 0; y < height; y++ ){
+						for ( var gridY = 0; gridY < rows.length; gridY++ ){
+							newRows.push(rows[gridY]/height);
+						}
+					}
+					for ( var x = 0; x < width; x++ ){
+						for ( var gridX = 0; gridX < cols.length; gridX++ ){
+							newCols.push(cols[gridX]/width);
+						}
+					}
+                    
+					windowModel.width = width;
+					windowModel.height = height;
+					windowModel.rows = newRows;
+					windowModel.cols = newCols;
+					windowModel.ratio *= width/height;
+					slideWidth = width;
+					slideHeight = height;
+					resizeGrid(that,windowModel);
+				}
+			});
+		}else{
+			resizeGrid(that,windowModels[$(that).attr("window-model")]);
+		}
+    }else{
+		var slide = slides[$(this).attr('id')];
+    	resizeGrid(that,windowModels[slide.windowModel])
+    }    
+}
+
 function createCanvasForWrapper (){
 	var that = this;
 	var group = $(that).parents(".group");
@@ -64,7 +142,7 @@ function createCanvasForWrapper (){
 				if ( $(that).hasClass("window_canvas") ){
 					createGrid(that,windowModel);
 					if ( $(that).attr("slide-id") ){
-						$.getJSON("/slide",{id:$(that).attr("slide-id")}, function (slide){
+						getSlideInCache($(that).attr("slide-id"), function (slide){
 							for(var i = 0; i < slide.relems.length; i++ ){
 								slide.relems[i].x -= ( parseInt($(that).attr("window-x")) - parseInt($(that).attr("slide-base-x")) )*windowModel.cols.length;
 								slide.relems[i].y -= ( parseInt($(that).attr("window-y")) - parseInt($(that).attr("slide-base-y")) )*windowModel.rows.length;
@@ -79,6 +157,7 @@ function createCanvasForWrapper (){
 					var newCols = [];
 					var width = $(that).attr("slide-width");
 					var height = $(that).attr("slide-height");
+                    
 					for ( var y = 0; y < height; y++ ){
 						for ( var gridY = 0; gridY < rows.length; gridY++ ){
 							newRows.push(rows[gridY]/height);
@@ -89,6 +168,7 @@ function createCanvasForWrapper (){
 							newCols.push(cols[gridX]/width);
 						}
 					}
+                    
 					windowModel.width = width;
 					windowModel.height = height;
 					windowModel.rows = newRows;
@@ -97,7 +177,7 @@ function createCanvasForWrapper (){
 					slideWidth = width;
 					slideHeight = height;
 					createGrid(that,windowModel);
-					$.getJSON("/slide",{id:$(that).attr("slide-id")}, function (slide){
+					getSlideInCache($(that).attr("slide-id"), function (slide){
 						addRelems(grids[$(that).attr("slide-id")],slide);
 					});
 				}
@@ -121,6 +201,18 @@ function getWindowByXY(x,y){
 	return elem;
 }
 
+function debouncer( func , timeout ) {
+   var timeoutID , timeout = timeout || 200;
+   return function () {
+      var scope = this , args = arguments;
+      clearTimeout( timeoutID );
+      timeoutID = setTimeout( function () {
+          func.apply( scope , Array.prototype.slice.call( args ) );
+      } , timeout );
+   }
+}
+
+
 $(document).ready(function(){
     var grids = new Object();
 	$(".action_button").each(function (){
@@ -136,10 +228,11 @@ $(document).ready(function(){
 		})
 	});
     $(".renderer_canvas").each(createCanvasForWrapper);
-	$(window).resize(function (){
-		console.log("resize")
-		$(".renderer_canvas").each(createCanvasForWrapper);
-	})
+	$(window).resize( function (e) {
+        // do stuff 
+        console.log("resize")
+        $(".renderer_canvas").each(createCanvasForWrapperLight);
+    })
 	$(".window.thumbnail").droppable({
 		accept:'.thumbnail.sequence, .thumbnail.slide',
 		over: function (event, ui){
