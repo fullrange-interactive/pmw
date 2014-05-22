eafe38e345a43cf69ba084acbb0ee8724f6fdc09
floriansegginger <f@floriansegginger.ch>
2014-05-22 17:27:25 +0200

    moved window server to port 8001

--- server/routes/drawing.js
+++ server/routes/drawing.js
@@ -30,6 +30,7 @@ exports.index = function(req, res){
     }
     if ( req.body.action == "newDrawing" ){
         var newDrawing = new Drawing();
+		console.log(req.body);
 		if ( req.body.background ){
         	newDrawing.backgroundImage = "http://" + config.url + req.body.background;
 		}else{
