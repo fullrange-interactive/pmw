3e13554a06fbd3db42915a668b46b4c7b4ca51c1
Ubuntu <ubuntu@ip-172-31-46-224.eu-west-1.compute.internal>
2016-03-29 15:28:50 +0000

    Change path in run_node.sh

--- server/run_node.sh
+++ server/run_node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do 
-cd /home/blroot/pmw/server/
-node /home/blroot/pmw/server/app.js > /var/log/pmw/node.log	
+cd /home/ubuntu/pmw/server/
+node /home/ubuntu/pmw/server/app.js > /var/log/pmw/node.log	
 sleep 1
 done
