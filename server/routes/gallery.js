c42be4236b72470b0e83519fc4125f4f774ecdf8
Kevin <kevin@henzer.ch>
2016-07-20 13:07:32 +0200

    Add auto-delete

--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -61,7 +61,8 @@ exports.index = function(req, res){
 
         walker.on('end', function() {
 
-            files.filter(function(element){return actualFileList.indexOf(element) >= 0});
+            files.filter(function(element){var ispresent = (actualFileList.indexOf(element) >= 0);
+                console.log("File "+element+"is present? "+ispresent;return ispresent; )});
 
             res.send(JSON.stringify(files));
         });
