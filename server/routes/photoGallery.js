8190747129f3a76f73275f29640228c107f6d857
Kevin <kevin@henzer.ch>
2016-07-20 11:32:35 +0200

    Prevent partial pictures to be displayed

---
+++ server/routes/photoGallery.js
@@ -0,0 +1,43 @@
+var fs = require('fs')
+var url = require('url')
+var gm = require('gm')
+var exec = require('child_process').exec;
+
+exports.index = function(req, res){
+    var walk    = require('walk');
+    var files   = [];
+    
+    res.header("Access-Control-Allow-Origin","*")
+    if ( req.query.listImages ){
+        // Walker options
+        var walker = walk.walk('public/gallery-bcvs/', { followLinks: false});
+
+        walker.on('file', function(root, stat, next) {
+            // Add this file to the list of files
+            if(stat.name == ".DS_Store"){
+                next();
+                return;
+            }
+
+			exec('gm identify -verbose ' + root + '/' + stat.name, function(error, stdout, stderr){
+	
+				if(stderr == '')
+		            files.push(root.replace("public","") + '' + stat.name);
+
+			});
+
+            next();
+        });
+
+        walker.on('end', function() {
+            res.send(JSON.stringify(files));
+        });
+    }else if ( req.query.imageUrl ){
+        AutomatorManagerInstance.AddSlideToGroupQueue(Configuration.photoGallerySlideId,Configuration.photoGalleryGroupId,{
+            url: req.query.imageUrl
+        });
+        res.send(JSON.stringify({responseType:'ok'}));
+    }else{
+        res.send();
+    }
+}
